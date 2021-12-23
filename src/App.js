ca53e9095f60b203687db360002d8223f1d348b1
Eric Crain <ericcrain77@gmail.com>
2021-12-22 19:18:26 -0500

    catching up to snapshot

--- src/App.js
+++ src/App.js
@@ -13,17 +13,18 @@ function App() {
     { name: 'food', description: 'Delicious delicacies' },
     { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' },
   ]);
+
   const [currentCategory, setCurrentCategory] = useState(categories[0]);
 
   return (
-    <div >
+    <div>
       <Nav
         categories={categories}
         setCurrentCategory={setCurrentCategory}
         currentCategory={currentCategory}
-        ></Nav>
+      ></Nav>
       <main>
-        <Gallery></Gallery>
+        <Gallery currentCategory={currentCategory}></Gallery>
         <About></About>
       </main>
     </div>
